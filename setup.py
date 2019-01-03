d805f0a98fe2eb073a56c964dded94b553122031
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2019-01-03 18:28:20 +0100

    templates are missing from the egg :/

--- setup.py
+++ setup.py
@@ -8,12 +8,24 @@ def requirements():
                 if dep.strip() != '' and not dep.startswith('-e')]
 
 
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
 setup(name='lair',
       version=__version__,
       packages=find_packages(),
-      zip_safe=False,
+      author="Balazs Nadasdi",
+      author_email="balazs.nadasdi@cheppers.com",
+      long_description=long_description,
+      url="https://github.com/yitsushi/lair",
+      zip_safe=True,
       include_package_data=True,
       install_requires=requirements(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+          "Operating System :: OS Independent",
+      ],
       entry_points="""
       [console_scripts]
       lair = lair.run:main
